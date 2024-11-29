9f246126d97f36ddec83ae01a10778a95e463d6e
DustReliant <1619513467@qq.com>
2024-11-30 01:12:32 +0800

    1.代码走查

--- luasrc/controller/filetransfer.lua
+++ luasrc/controller/filetransfer.lua
@@ -71,3 +71,144 @@ function action_submit()
     -- 清理临时 CSRF Token 文件
     clear_csrf_token()
 end
+
+
+
+function action_start()
+	luci.http.prepare_content("application/json")
+	luci.http.write_json({
+			startlog = startlog();
+	})
+end
+
+
+function action_refresh_log()
+	luci.http.prepare_content("application/json")
+	local logfile="/tmp/filetransfer.log"
+	local file = io.open(logfile, "r+")
+	local info, len, line, lens, cache, ex_match, line_trans
+	local data = ""
+	local limit = 1000
+	local log_tb = {}
+	local log_len = tonumber(luci.http.formvalue("log_len")) or 0
+	if file == nil then
+ 		return nil
+ 	end
+ 	file:seek("set")
+ 	info = file:read("*all")
+ 	info = info:reverse()
+ 	file:close()
+ 	cache, len = string.gsub(info, '[^\n]+', "")
+ 	if len == log_len then return nil end
+	if log_len == 0 then
+		if len > limit then lens = limit else lens = len end
+	else
+		lens = len - log_len
+	end
+	string.gsub(info, '[^\n]+', function(w) table.insert(log_tb, w) end, lens)
+	for i=1, lens do
+		line = log_tb[i]:reverse()
+		line_trans = line
+		ex_match = false
+		core_match = false
+		time_format = false
+		while true do
+			ex_keys = {"UDP%-Receive%-Buffer%-Size", "^Sec%-Fetch%-Mode", "^User%-Agent", "^Access%-Control", "^Accept", "^Origin", "^Referer", "^Connection", "^Pragma", "^Cache-"}
+			for key=1, #ex_keys do
+				if string.find (line, ex_keys[key]) then
+					ex_match = true
+					break
+				end
+			end
+    		if ex_match then break end
+
+			core_keys = {" DBG ", " INF ", "level=", " WRN ", " ERR ", " FTL "}
+			for key=1, #core_keys do
+				if string.find(string.sub(line, 0, 13), core_keys[key]) or (string.find(line, core_keys[key]) and core_keys[key] == "level=") then
+					core_match = true
+					if core_keys[key] ~= "level=" then
+						time_format = true
+					end
+					break
+				end
+			end
+			if time_format then
+				if string.match(string.sub(line, 0, 8), "%d%d:%d%d:%d%d") then
+					line_trans = '"'..os.date("%Y-%m-%d %H:%M:%S", tonumber(string.sub(line, 0, 8)))..'"'..string.sub(line, 9, -1)
+				end
+			end
+			if not core_match then
+				if not string.find (line, "【") or not string.find (line, "】") then
+					line_trans = trans_line_nolabel(line)
+				else
+					line_trans = trans_line(line)
+				end
+			end
+			if data == "" then
+				data = line_trans
+			elseif log_len == 0 and i == limit then
+				data = data .."\n" .. line_trans .. "\n..."
+			else
+				data = data .."\n" .. line_trans
+			end
+    		break
+    	end
+	end
+	luci.http.write_json({
+		len = len,
+		log = data;
+	})
+end
+
+function action_del_log()
+	luci.sys.exec(": > /tmp/filetransfer.log")
+	return
+end
+
+function action_del_start_log()
+	luci.sys.exec(": > /tmp/filetransfer_start.log")
+	return
+end
+
+function action_log_level()
+	local level, info
+	if is_running() then
+		local daip = daip()
+		local dase = dase() or ""
+		local cn_port = cn_port()
+		if not daip or not cn_port then return end
+		info = json.parse(luci.sys.exec(string.format('curl -sL -m 3 -H "Content-Type: application/json" -H "Authorization: Bearer %s" -XGET http://"%s":"%s"/configs', dase, daip, cn_port)))
+		if info then
+			level = info["log-level"]
+		else
+			level = uci:get("filetransfer", "config", "log_level") or "info"
+		end
+	else
+		level = uci:get("filetransfer", "config", "log_level") or "info"
+	end
+	luci.http.prepare_content("application/json")
+	luci.http.write_json({
+		log_level = level;
+	})
+end
+
+function action_switch_log()
+	local level, info
+	if is_running() then
+		local daip = daip()
+		local dase = dase() or ""
+		local cn_port = cn_port()
+		level = luci.http.formvalue("log_level")
+		if not daip or not cn_port then luci.http.status(500, "Switch Faild") return end
+		info = luci.sys.exec(string.format('curl -sL -m 3 -H "Content-Type: application/json" -H "Authorization: Bearer %s" -XPATCH http://"%s":"%s"/configs -d \'{\"log-level\": \"%s\"}\'', dase, daip, cn_port, level))
+		if info ~= "" then
+			luci.http.status(500, "Switch Faild")
+		end
+	else
+		luci.http.status(500, "Switch Faild")
+	end
+	luci.http.prepare_content("application/json")
+	luci.http.write_json({
+		info = info;
+	})
+end
