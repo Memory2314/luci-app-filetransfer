8ae99cf696e9f4fbce11c5268887d38f71847b03
DustReliant <1619513467@qq.com>
2024-12-01 21:41:41 +0800

    1.调整文件

---
+++ luasrc/model/cbi/filetransfer/log.lua
@@ -0,0 +1,22 @@
+--
+local NXFS = require "nixio.fs"
+local SYS  = require "luci.sys"
+local HTTP = require "luci.http"
+
+m = Map("filetransfer", translate("Server Logs"))
+s = m:section(TypedSection, "filetransfer")
+m.pageaction = false  -- 不显示保存和应用按钮
+s.anonymous = true
+s.addremove = false
+
+log = s:option(TextValue, "clog")
+log.readonly = true
+log.pollcheck = true
+log.template = "cbi/log"
+log.description = translate("")
+log.rows = 29
+
+-- m:append(Template("toolbar_show"))
+m:append(Template("cbi/config_editor"))
+
+return m
