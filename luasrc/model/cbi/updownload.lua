84dac9dfabb0006d6171ec2ae0ab8707bf7be0ee
DustReliant <1619513467@qq.com>
2024-12-07 00:27:22 +0800

    1.添加日志

--- luasrc/model/cbi/updownload.lua
+++ luasrc/model/cbi/updownload.lua
@@ -151,6 +151,13 @@ if http.formvalue("upload") then
     if not validate_csrf_token(csrf_token_from_form) then
         um.value = translate("Invalid CSRF token!")
         write_log("CSRF token validation failed for upload action.")
+    else
+        local upload_file = http.formvalue("ulfile")  -- 读取上传文件字段
+        if not upload_file or #upload_file == 0 then
+            local msg = translate("No file specified for upload.")
+            um.value = msg
+            write_log(msg)  -- 记录日志
+        end
     end
 elseif http.formvalue("download") then
     local csrf_token_from_form = http.formvalue("csrf_token")  -- 读取表单中的 CSRF Token
@@ -158,9 +165,17 @@ elseif http.formvalue("download") then
         dm.value = translate("Invalid CSRF token!")
         write_log("CSRF token validation failed for download action.")
     else
-        download_file()
+        local download_path = http.formvalue("dlfile")  -- 读取下载文件路径字段
+        if not download_path or #download_path == 0 then
+            local msg = translate("No file path specified for download.")
+            dm.value = msg
+            write_log(msg)  -- 记录日志
+        else
+            download_file()
+        end
     end
 end
+
 
 -- 获取上传目录文件列表
 local inits = {}
